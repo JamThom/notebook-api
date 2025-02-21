4433b66c7309501ec2744388b2ad2093f3b4bcca
jt <jt@jts-macbook-air.home>
2025-02-21 13:12:17 +0000

    try1

--- Hubs/Page.cs
+++ Hubs/Page.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.SignalR;
+using Notebook.Data;
 using Notebook.Models;
 
 namespace Notebook.Hubs
 {
     public class PageHub: Hub
     {
+        private readonly DbContext _context;
+        public PageHub(DbContext context)
+        {
+            _context = context;
+        }
         public async Task UpdatePage(Page page)
         {
+            _context.Pages.Update(page);
+            await _context.SaveChangesAsync();
             await Clients.All.SendAsync("UpdatePage", page);
         }
     }
