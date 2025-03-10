a6cb4c85eb7694e87813195687a1e8dcb6bca9bc
jt <jt@jts-MacBook-Air.local>
2025-03-10 15:09:56 +0100

    bilf ficks

--- Program.cs
+++ Program.cs
@@ -73,6 +73,17 @@ var app = builder.Build();
 
 app.UseCors("AllowSpecificOrigins");
 
+// Enable middleware to serve generated Swagger as a JSON endpoint.
+app.UseSwagger();
+
+// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+// specifying the Swagger JSON endpoint.
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
